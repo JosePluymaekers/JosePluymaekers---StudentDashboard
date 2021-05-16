7e3fb3d0344176bcbeaadf7570de0a8ebe6efb20
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-05-16 14:29:23 +0200

    Add files via upload

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReviewList from "./ReviewList";
+
+class Container extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <ReviewList />
+      </div>
+    );
+  }
+}
+
+export default Container;
