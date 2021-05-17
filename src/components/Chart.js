3a24df751efe0b8ed3d8463ce01bfd42cb1935a1
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-05-17 22:58:31 +0200

    Add files via upload

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,69 @@
 import React from "react";
 
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLabel,
+} from "victory";
+
 class Chart extends React.Component {
   render() {
-    const reviews = this.props.reviews;
-    console.log(reviews);
+    const tasks = this.props.tasks;
+    console.log(tasks);
 
     return (
       <div>
         <h1>chart here</h1>
+        <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
+          <VictoryAxis
+            style={{
+              ticks: {
+                fill: "transparent",
+                size: 2,
+                stroke: "black",
+                strokeWidth: 1,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+              },
+            }}
+            tickLabelComponent={
+              <VictoryLabel
+                angle={() => {
+                  return this.props.tasks.length < 16 ? 0 : 60;
+                }}
+                dx={-5.5}
+                dy={-4}
+                style={{
+                  fontSize: 4,
+                  fill: "#223243",
+                }}
+                textAnchor={"start"}
+              />
+            }
+          />
+
+          <VictoryAxis
+            dependentAxis
+            // tickFormat specifies how ticks should be displayed
+            tickValues={[1, 2, 3, 4, 5]}
+            tickFormat={(x) => `${x / 1}`}
+            style={{
+              tickLabels: {
+                fontSize: 4,
+                fill: "#223243",
+              },
+            }}
+          />
+          <VictoryBar
+            data={tasks}
+            // data accessor for x values
+            x={"task"}
+            // // data accessor for y values
+            y={"averageDifficulty"}
+          />
+        </VictoryChart>
       </div>
     );
   }
--- src/components/Container.js
+++ src/components/Container.js
@@ -8,22 +8,50 @@ class Container extends Component {
     super();
     this.state = {
       reviews: reviewsData,
+      tasks: [],
     };
-    console.log(this.state.reviews);
   }
 
-  // getAverage(rating) {
-  //   for (var i = 0; i < this.state.reviews.difficultyRating.length; i++) {
-  //     sum += parseInt(grades[i], 10);
-  //   }
-  //   var avg = sum / grades.length;
-  //   return avg;
-  // }
+  componentDidMount() {
+    let tasks = [];
+    this.state.reviews.forEach((item) => {
+      if (tasks.indexOf(item.task) === -1) {
+        tasks.push(item.task);
+      }
+    });
+
+    const updatedTasks = tasks.map((task) => {
+      let difficulty = 0;
+      let enjoyment = 0;
+      let labels = [];
+
+      this.state.reviews.forEach((item) => {
+        if (task === item.task) {
+          difficulty += parseInt(item.difficultyRating);
+          enjoyment += parseInt(item.enjoymentRating);
+          labels.push(item);
+        }
+      });
+      const newObj = {
+        task: task,
+        averageDifficulty: difficulty / labels.length,
+        averageEnjoy: enjoyment / labels.length,
+        labels: labels,
+      };
+      return newObj;
+    });
+    this.setState((prevState) => {
+      const updateState = { ...prevState };
+      updateState.tasks = updatedTasks;
+      return updateState;
+    });
+  }
 
   render() {
+    console.log(this.state.tasks);
     return (
       <div>
-        <Chart reviews={this.state.reviews} />
+        <Chart tasks={this.state.tasks} />
         <ReviewTotalTable reviews={this.state.reviews} />
       </div>
     );
